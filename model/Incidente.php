badca46bccda25a32ab49080d12e2bac0913be0f
joshuanavarrovelasquez-desing <joshuanavarrovelasquez@gmail.com>
2026-09-14 19:10:55 -0500

    heredaremos de Ticket y define cómo se comporta un problema técnico urgente

---
+++ model/Incidente.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'Ticket.php';
+
+// HERENCIA: Incidente hereda de Ticket
+class Incidente extends Ticket {
+    
+    // Propiedad específica solo para incidentes
+    private string $nivelImpacto; 
+
+    public function __construct(string $usuarioSolicitante, string $nivelImpacto) {
+        // Llamamos al constructor de la clase padre (Ticket)
+        parent::__construct($usuarioSolicitante);
+        $this->nivelImpacto = $nivelImpacto;
+    }
+
+    // POLIMORFISMO: Implementamos el método abstracto a la manera del Incidente
+    public function calcularTiempoResolucion(): string {
+        if ($this->nivelImpacto === 'Alta') {
+            return "2 horas (SLA Crítico)";
+        }
+        return "24 horas (SLA Normal)";
+    }
+}
+?>
